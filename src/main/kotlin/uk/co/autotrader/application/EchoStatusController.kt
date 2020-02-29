29ef1781af1eac12bd04143387ad25b3e974e536
Nick Ebbitt <nickebbitt@gmail.com>
2020-02-29 11:20:24 +0000

    Capture specific exception for invalid echo status codes

--- src/main/kotlin/uk/co/autotrader/application/EchoStatusController.kt
+++ src/main/kotlin/uk/co/autotrader/application/EchoStatusController.kt
@@ -1,6 +1,7 @@
 package uk.co.autotrader.application
 
 import io.micrometer.core.annotation.Timed
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,12 +14,15 @@ import org.springframework.web.bind.annotation.RestController
 @Timed
 class EchoStatusController {
 
+    private val LOG = LoggerFactory.getLogger(EchoStatusController::class.java)
+
     @GetMapping("/{status}")
     fun echoStatus(@PathVariable status: String): ResponseEntity<Any> {
         val httpStatus = try {
             HttpStatus.valueOf(status.toInt())
-        } catch (e: Exception) {
-            return ResponseEntity.badRequest().body(e.message)
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            LOG.error("Failed to parse status: $status", illegalArgumentException)
+            return ResponseEntity.badRequest().body("Failed to parse provided HTTP status code: $status")
         }
 
         return ResponseEntity(httpStatus)
