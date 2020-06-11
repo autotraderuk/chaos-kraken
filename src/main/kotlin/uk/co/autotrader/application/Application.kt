a9ebdfb2f54b10f214ef624376b931e566ddefbf
Nick Ebbitt <nickebbitt@gmail.com>
2020-06-11 12:21:47 +0100

    Bump to SpringBoot 2.3.0 & Gradle 6.5

--- src/main/kotlin/uk/co/autotrader/application/Application.kt
+++ src/main/kotlin/uk/co/autotrader/application/Application.kt
@@ -3,7 +3,6 @@ package uk.co.autotrader.application
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import javax.annotation.PostConstruct
 
 @SpringBootApplication
